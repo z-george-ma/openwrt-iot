cd4585839cb5bd58b2d28399a9d8cc358ebc9880
George Z Ma <z.george.ma@gmail.com>
2018-10-22 21:28:44 +1100

    Update config.lua

--- src/config.lua
+++ src/config.lua
@@ -6,7 +6,7 @@ local cursor = uci.cursor()
 
 local modulePath = cursor.get('iot', 'common', 'modules') or '/usr/lib/lua/iot/modules/'
 
-cursor:foreach("iot", "actuator", function(s)
+cursor:foreach("iot", "module", function(s)
   local name = s['.name']
   M[name] = {}
   M[name].config = s
